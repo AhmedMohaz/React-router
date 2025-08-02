195f8d5b3216b0c8f27762b89afa05567830987f
Ahmed Mohamed <itslokin5@gmail.com>
2025-08-02 18:13:41 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MovieList from "./components/MovieList";
-import MovieDescription from "./components/MovieDescription";
+import MovieList from "./MovieList";
+import MovieDescription from "./MovieDescription";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
