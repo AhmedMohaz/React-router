cab6390058204577c73e5feb0131759ef0ac773e
Ahmed Mohamed <itslokin5@gmail.com>
2025-08-02 18:08:08 +0300

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,21 @@
+
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    description: "A mind-bending thriller by Christopher Nolan.",
+    posterURL: "https://m.media-amazon.com/images/I/51FN6VZ6PZL._AC_.jpg",
+    rating: 9,
+    trailer: "https://www.youtube.com/embed/YoHD9XEInc0"
+  },
+  {
+    id: 2,
+    title: "Interstellar",
+    description: "A journey beyond the stars.",
+    posterURL: "https://m.media-amazon.com/images/I/71n58xDCOdL._AC_SY679_.jpg",
+    rating: 8.5,
+    trailer: "https://www.youtube.com/embed/zSWdZVtXT7E"
+  }
+];
+
+export default movies;
